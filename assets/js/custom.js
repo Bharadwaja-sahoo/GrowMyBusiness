32462d22e7afd016d9f8104ccfcac0c16d01878d
Bharadwaja-sahoo <bharadwajasahoo96@gmail.com>
2026-08-02 21:49:27 +0530

    .

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -103,7 +103,7 @@ planhover.forEach((hoverplan) => {
 
 // 1.14. Talk section ===  form Submit massage
 // PASTE your Google Apps Script Web App URL here (see setup guide provided with these files)
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyoaIfnvh2cB3xqTufZPzaeoBObE0SdNsqDxH7CBYYa_RNAwK5j-6DDtOiY97WxAesYgg/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwiGzmIMmAZ_xJgcyRqK9jsK1hrYbeJN0bmldrRHCSZ1vmKKaBCkrnUiTtqtB7X7WvH/exec";
 
 const aboutFor = document.getElementById('Subscribe-massage');
 const aboutMessag = document.getElementById('Succes-box');
